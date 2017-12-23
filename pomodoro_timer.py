126b0264c252606b8f213e122e7f3d265ff30a9c
JosephVC <josephvcardenas@gmail.com>
2017-12-23 15:02:44 -0800

    Commit after figuring out logic issue

--- pomodoro_timer.py
+++ pomodoro_timer.py
@@ -7,7 +7,6 @@ so far.
 You also have the option of setting the transparency of the app, in case you
 want it to remain visible in the corner of your screen.
 """
-
 from tkinter import *
 from tkinter import ttk
 
@@ -15,11 +14,18 @@ from tkinter import ttk
 root = Tk()
 root.title("Pomodoro Timer")
 
-# state variables
 
-entered_time = StringVar()
-statusmsg = StringVar()
-remaining = 0
+# Define the widgets and, for now, pack them in to the display
+
+time = ttk.Entry(root).pack()
+start_button = Entry(root).pack()
+status = ttk.Label(root).pack()
+
+# Grid the widgets
+# TODO -- everything is .pack()-d in, per the above.
+#         This works well enough for now, but as the app expands, widgets will
+#         need to be well-gridded.
+
 
 # create the countdown timer
 # if a number is entered in the time box and the user hits the start_button,
@@ -29,48 +35,18 @@ remaining = 0
 # so, the time ttk.Entry above should actually be the time, with the logic
 # working with whatever the user enters
 def countdown():
-    # if time field has content
-    # if start_button has been pressed
-    # display time time running down
+    def _inner():
+        countdown_from = int(time.get())
+        while countdown_from > 0:
+            status.configure(text=countdown_from)
+            time.sleep(1)
+            countdown_from -= 1
+            if countdown_from <= 0:
+                display_box.configure(text="Time's Up.")
+    thread = threading.Thread(target=_inner)
+    thread.start()
 
-    if time is not None:
-        if remaining is not None:
-            remaining = remaining
-        if remaining <= 0:
-            statusmsg.configure(text="TIME'S UP")
-        else:
-            statusmsg.configure(text="{}".format(remaining))
-            remaining -= 1
-
-# Define the widgets and their qualities
-content = ttk.Frame(root, padding=(3,3, 12,12))
-frame = ttk.Frame(content, borderwidth=5, relief="sunken", width=500,
-height=400)
-text1 = ttk.Label(content, text="Time", width=4)
-#time_up = ttk.Label(content, text="Time Up")
-#  QUESTION: HOW DO I PASS USER INPUT TO AN ENTRY BOX
-#  ANSWER: make the text entry box a ttk.Entry box attached to a textvariable
-
-time = ttk.Entry(content, width=7, textvariable=entered_time)
-start_button = ttk.Button(content, text="START", command=countdown)
-stop_button = ttk.Button(content, text="STOP")
-status = ttk.Label(content, textvariable = statusmsg, anchor= W )
-
-
-# Grid the widgets
-# TODO -- everything is very spread out, better structure grid
-content.grid(column=0, row=0, sticky=(N,S,E,W))
-#content.columnconfigure(3, weight=5)
-frame.grid(column=0, row=0, columnspan=3, rowspan=3, sticky=(N,S,E,W))
-text1.grid(column=1, row=0, columnspan=1)
-time.grid(column=0, row=0, columnspan=1)
-start_button.grid(column=0, row=1, columnspan=1)
-stop_button.grid(column=0, row=2, columnspan=1)
-status.grid(column=1, row=3, columnspan=1)
-
-# set event bindings
-start_button.bind('<Double-1>', countdown)
-# stop_button.bind(pause the countdown )
+countdown_button = Button(root, text="countdown", command=countdown).pack()
 
 # run mainloop
 root.mainloop()
